32b5f8e4b0ac35d19d8a4b096c8e30b5247b1122
Daniel Ramirez <zet.daniel@outlook.com>
2017-02-03 18:25:06 -0600

    remove

--- multimediapicker/src/main/java/com/zet/enterprises/multimediapicker/ContainerFragment.java
+++ multimediapicker/src/main/java/com/zet/enterprises/multimediapicker/ContainerFragment.java
@@ -1121,16 +1121,7 @@ public class ContainerFragment extends Fragment implements OnImageClickListener 
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+   
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
